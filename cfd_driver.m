764f994dd38099ca638517ee92138d44968252df
Syj-0525 <812368857@qq.com>
2022-05-22 19:54:57 +0800

    更新： 二次九结点单元拆分为四个线性单元

--- cfd_driver.m
+++ cfd_driver.m
@@ -143,4 +143,12 @@ x = K\B;
 ux_k_1 = x(1:Nz);
 vy_k_1 = x(1 + Nz: 2 * Nz);
 p4 = x(1 + 2 * Nz: 2 * Nz + Nd);
+p_k_1= [Pding2Pzong(p4, JMV)]';   % 压力插值计算
 %%%%%% 清理内存，求解方程
+
+%%%%%% 输出Tecplot后处理结果
+E = E * 4;
+Nz = Nz;
+data = [JXYV, ux_k_1, vy_k_1, sqrt(ux_k_1.^2 + vy_k_1.^2), p_k_1];
+JMV4 = JMV_9to4(JMV);    % 二次九结点单元拆分为四个线性单元, 并建立IEN
+%%%%%% 输出Tecplot后处理结果
